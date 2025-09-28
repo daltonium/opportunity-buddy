40a7c48d9319ec4dc1f92a1c60055c77ccb8fd91
Mohith Dalton <167634117+daltonium@users.noreply.github.com>
2025-09-28 22:51:42 +0530

    going headless, updated csv file

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def main(position, location):
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
 
     driver = webdriver.Chrome(options=options)
 
